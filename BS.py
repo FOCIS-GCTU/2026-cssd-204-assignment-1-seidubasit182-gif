5a148d5683b81b7f81c2aba0b3439a85169ab202
seidubasit182-gif <seidubasit182@gmail.com>
2026-05-30 23:35:15 +0000

    BS.py
    Print out out of initials

---
+++ BS.py
@@ -0,0 +1,33 @@
+# File: BS.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: BASIT SEIDU
+# STUDENT ID:  2425400270
+# Email: SEIDUBASIT182@GMAIL.COM
+# Grader: MR. AUGUSTUS BUCKMAN
+#
+# On my honor, BASIT SEIDU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials.
+    print()
+    print("...BSB")
+    print()
+    print("...BBBBBBBBBB............SSSSS..........BBBBBBBBBB...")
+    print("...B.........B.........SS...............B.........B...")
+    print("...B..........B........SS...............B..........B...")
+    print("...B.........B...........SS.............B.........B...")
+    print("...BBBBBBBBBB..............SS...........BBBBBBBBBB...")
+    print("...B.........B................SS........B.........B...")
+    print("...B..........B...............SS........B..........B...")
+    print("...B..........B.............SS..........B..........B...")
+    print("...B.........B....**.......SS......**...B.........B...**")
+    print("...BBBBBBBBBB.....**....SSSS.......**...BBBBBBBBBB...**")
+    print()
+
+
+
+main()
